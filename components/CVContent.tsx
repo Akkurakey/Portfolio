9ca507d807274074ae4b94ba570722a2fd3ad1b5
Akkurakey <92529866+Akkurakey@users.noreply.github.com>
2026-06-11 17:42:39 +0100

    Update CVContent.tsx

--- components/CVContent.tsx
+++ components/CVContent.tsx
@@ -192,15 +192,15 @@ const CVContent: React.FC<CVContentProps> = ({ onOpenFolder, onOpenProjectById }
             <div className="space-y-6 font-sans text-[11px] leading-relaxed">
               <div>
                 <h4 className="font-black text-gray-300 mb-1.5 tracking-widest text-[9px] uppercase">Hci</h4>
-                <p className="text-gray-700">Experimental Design / User Studies / Questionnaire Design / Semi-structured Interview / Ethnographic Methods</p>
+                <p className="text-gray-700">User Studies / Ethnographic Methods</p>
               </div>
               <div>
                 <h4 className="font-black text-gray-300 mb-1.5 tracking-widest text-[9px] uppercase">Implement</h4>
-                <p className="text-gray-700">XR Prototyping (Unity, C#; Spark AR) / Git & Version Control</p>
+                <p className="text-gray-700">XR (Unity, C#; WebXR; Spark AR) / Git & Version Control</p>
               </div>
               <div>
                 <h4 className="font-black text-gray-300 mb-1.5 tracking-widest text-[9px] uppercase">Research</h4>
-                <p className="text-gray-700">Mixed-methods Research (Qual/Quant) / Data Analysis (SPSS) / Academic Writing / Data Visualisation (Academic Poster)</p>
+                <p className="text-gray-700">Research-through-Design / Mixed-methods Research (Qual/Quant) / Data Analysis (SPSS/Python) / Data Visualisation</p>
               </div>
               <div>
                 <h4 className="font-black text-gray-300 mb-2 tracking-widest text-[9px] uppercase">Design</h4>
